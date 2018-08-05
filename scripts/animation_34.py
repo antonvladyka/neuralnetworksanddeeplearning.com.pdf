6896d49c8b2036fd55d9b2b75c363d2afeb96611
Anton <antonvladyka@gmail.com>
2018-08-05 13:25:25 +0100

    Chapter3, sections 3.1-3.3

--- scripts/animation_34.py
+++ scripts/animation_34.py
@@ -7,14 +7,14 @@ Created on Fri Aug  3 20:04:02 2018
 
 # chapter 3, animation 1
 import math
-def cost(x):    return x**2/2
-def costd(x):   return x*x*(1-x)
+def cost(x):    return -math.log(1-x)
+def costd(x):   return 1/(1-x)
 def s(x):       return 1/(1+math.exp(-x))
 def ov(w,b):    return s(w+b)
 x = 1
-w0 = 0.6
-b0 = 0.9
-eta = 0.15
+w0 = 2
+b0 = 2
+eta = 0.005
 w = w0
 b = b0
 Y = []
@@ -36,22 +36,23 @@ while x <= 300:
     x = x + 1
 
 import matplotlib.pyplot as plt
+plt.figure(figsize=(10,5))
 plt.subplot(2,2,1)
 plt.plot(X,Y)
-plt.xlabel('Epoch')
-plt.legend(['Cost'])
+plt.legend(['Cost'],loc=0)
 plt.subplot(2,2,3)
-plt.plot(X,A)
+plt.plot(X,A,'red')
 plt.xlabel('Epoch')
-plt.legend(['Output'])
+plt.legend(['Output'],loc=0)
 plt.subplot(2,2,2)
-plt.plot(X,W)
-plt.xlabel('Epoch')
-plt.legend(['Weight'])
+plt.plot(X,W,'green')
+plt.legend(['Weight'],loc=0)
 plt.subplot(2,2,4)
-plt.plot(X,B)
+plt.plot(X,B,'black')
 plt.xlabel('Epoch')
-plt.legend(['Bias'])
+plt.legend(['Bias'],loc=0)
+plt.tight_layout()
+plt.savefig('animation_33',dpi=300,pad_inches=0,bbox_inches=0)
 #plt.plot(X,B)
 #plt.plot(X,W)
 
